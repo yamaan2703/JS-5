f3862289c5686f3f216849462bd3466a39edfba1
yamaan2703 <115937602+yamaan2703@users.noreply.github.com>
2022-11-24 03:36:44 +0500

    commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// Changing Case
+
+// var letter = prompt("Enter a Letters")
+// switch(letter){
+//     case "apple":
+//     alert("its a Fruit")
+//     default:
+//         console.log("Not Found")
+// }
+
+
+// Date & Time
+
+// var dt = new Date()
+// console.log(dt)
+
+// var dt = new Date()
+// var a = dt.getDay()
+// alert(a)
+
+if ("Sat"){
+    prompt("Day")
+    alert("its fun day")
+}
+else{
+    alert("Not Found")
+}
